29933a30b070550b8b2683bfa55183f414bb4291
Conor Sayres <csayres@uw.edu>
2020-02-04 16:06:55 -0800

    initial connection.

--- python/baslerCam.py
+++ python/baslerCam.py
@@ -1,3 +1,26 @@
-import pypylon
+from pypylon import pylon
 
 from basecam import CameraSystem, BaseCamera, CameraEvent
+
+
+
+
+
+# list devices:
+def listDevices():
+    serialNums = []
+    for dev in pylon.TlFactory.GetInstance().EnumerateDevices():
+        serialNums.append(dev.GetSerialNumber())
+    return serialNums
+
+def openDevice(serialNumber):
+    info = pylon.DeviceInfo()
+    info.SetSerialNumber(serialNumber)
+    cam = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice(info))
+    cam.Open()
+    return cam
+
+devs = listDevices()
+print("found devices", devs)
+cam = openDevice(devs[0])
+print("horizontal bin", cam.BinningHorizontal.Value)
